6d13bd4c04c6718ecb8a243d577eda90229a3eec
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-25 01:04:24 +0100

    tested ajax request is being made (didn't manage to stub out the response which is not ideal

--- app/__test__/gotQuotesApi-test.js
+++ app/__test__/gotQuotesApi-test.js
@@ -1,23 +1,10 @@
 jest.unmock('../utils/gotQuotesApi')
-jest.unmock('axios')
 import {getQuote} from '../utils/gotQuotesApi'
-import MockAdapter from 'axios-mock-adapter'
-import axios from 'axios'
+import $ from 'jquery'
 
 describe('getQuote', () => {
-  it('fetches a quote from Api', () => {
-    let mockData = {
-      data: {
-        quote: 'I drink and I know things.',
-        character: 'Tyrion'
-      }
-    }
-    let mockAxios = new MockAdapter(axios)
-
-    const URL = 'https://got-quotes.herokuapp.com/quotes'
-    mockAxios.onGet(URL).reply(200, mockData)
-    return getQuote().then((result) => {
-      expect(result).toEqual(mockData.data)
-    })
+  it('makes an ajax request to the Api', () => {
+    getQuote()
+    expect($.get).toBeCalledWith('https://got-quotes.herokuapp.com/quotes', jasmine.any(Function))
   })
 })
