96011e6bff48946b9f1fce253c431f4986147512
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-26 19:14:16 +0100

    Added an input field and button for getting quotes by character

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,21 +1,40 @@
 import React, {PropTypes} from 'react'
 
+function GetQuoteByCharacter (props) {
+  return (
+    <div className='get-quote-by-char'>
+      <input
+        className='form-control'
+        placeholder='Jon Snow, Tyrion, Hodor' />
+      <button
+        className='btn btn-success'
+        onClick={props.onSubmitCharacter}>
+        Search by Character
+      </button>
+    </div>
+  )
+}
+
 function Home (props) {
   return (
     <div className='jumbotron text-center'>
-      <button
-        className='get-quote btn btn-success'
-        onClick={props.onGetQuote}>
-        New Quote
-      </button>
-      <p>{props.quote}</p>
-      <p>{props.character}</p>
+      <div className='inline-flex'>
+        <button
+          className='get-quote btn btn-success'
+          onClick={props.onGetQuote}>
+          Random Quote
+        </button>
+        <GetQuoteByCharacter onSubmitCharacter={props.onSubmitCharacter}/>
+      </div>
+      <p className='quote'>{props.quote}</p>
+      <p className='character'>{props.character}</p>
     </div>
   )
 }
 
 Home.propTypes = {
   onGetQuote: PropTypes.func.isRequired,
+  onSubmitCharacter: PropTypes.func.isRequired,
   quote: PropTypes.string.isRequired,
   character: PropTypes.string.isRequired
 }
