ef1c7eef084d1dd3751ec5d8dbfa47673cbeb9ec
Junyuan Xue <junyuanxue.is@gmail.com>
2016-07-04 21:19:57 +0100

    Added onChange functions in inputs

--- app/components/addQuote.js
+++ app/components/addQuote.js
@@ -5,17 +5,24 @@ function AddQuote (props) {
     <div className='jumbotron text-center add-quote-container'>
       <textarea
         className='form-control quote-input'
-        placeholder='Some epic quote here' />
+        placeholder='Some epic quote here'
+        onChange={props.onUpdateQuote} />
       <input
         className='form-control char-input'
-        placeholder='Who said it?' />
+        placeholder='Who said it?'
+        onChange={props.onUpdateCharacter} />
       <button
         className='btn btn-success submit-quote'
-        onClick={props.onSubmitCharacter}>
+        onClick={props.onSubmitQuote}>
         Submit
       </button>
     </div>
   )
+}
+
+AddQuote.propTypes = {
+  onUpdateQuote: PropTypes.func.isRequired,
+  onUpdateCharacter: PropTypes.func.isRequired
 }
 
 export default AddQuote
--- app/containers/AddQuoteContainer.js
+++ app/containers/AddQuoteContainer.js
@@ -2,9 +2,19 @@ import React from 'react'
 import AddQuote from '../components/AddQuote'
 
 class AddQuoteContainer extends React.Component {
+  handleUpdateQuote(event) {
+    console.log(event.target.value)
+  }
+
+  handleUpdateCharacter(event) {
+    console.log(event.target.value)
+  }
+
   render() {
     return (
-      <AddQuote />
+      <AddQuote
+        onUpdateQuote={this.handleUpdateQuote}
+        onUpdateCharacter={this.handleUpdateCharacter} />
     )
   }
 }
